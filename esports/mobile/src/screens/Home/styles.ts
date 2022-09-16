93959909dcea1acf5d2adea58d9baf9cc3e2b200
Isabelle Strojake <strojake@gmail.com>
2022-09-16 16:33:21 -0300

    mobile home layout with react

---
+++ esports/mobile/src/screens/Home/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  logo: {
+    width: 210,
+    height: 100,
+    marginTop: 34,
+    marginBottom: 19,
+  },
+
+  contentList: {
+    paddingLeft: 32,
+    paddingRight: 64,
+  }
+});
---
+++ esports/mobile/src/utils/games.ts
@@ -0,0 +1,38 @@
+export const GAMES = [
+  {
+    id: '1',
+    name: 'League of Legends',
+    ads: '4',
+    cover: require('../assets/games/league-of-legends.png')
+  },
+  {
+    id: '2',
+    name: 'Dota 2',
+    ads: '3',
+    cover: require('../assets/games/dota-2.png')
+  },
+  {
+    id: '3',
+    name: 'CS-GO',
+    ads: '2',
+    cover: require('../assets/games/counter-strike.png')
+  },
+  {
+    id: '4',
+    name: 'Apex',
+    ads: '3',
+    cover: require('../assets/games/apex.png')
+  },
+  {
+    id: '5',
+    name: 'Fortnite',
+    ads: '5',
+    cover: require('../assets/games/fortnite.png')
+  },
+  {
+    id: '6',
+    name: 'World of Warcraft',
+    ads: '2',
+    cover: require('../assets/games/world-of-warcraft.png')
+  },
+];
