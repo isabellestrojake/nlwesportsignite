2054aa83c3aa9374c8686078ef5a5fdb7d58a153
Isabelle Strojake <strojake@gmail.com>
2022-09-18 17:33:09 -0300

    integrated mobile and web with server

--- esports/mobile/src/screens/Home/index.tsx
+++ esports/mobile/src/screens/Home/index.tsx
@@ -1,13 +1,30 @@
-import { View, Image, FlatList } from 'react-native';
+import { Image, FlatList } from 'react-native';
 import { styles } from './styles';
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../components/Heading';
-import { GameCard } from '../../components/GameCard';
-import { GAMES } from '../../utils/games';
+import { GameCard, GameCardProps } from '../../components/GameCard';
+import { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Background } from '../../components/Background';
+import { useNavigation } from '@react-navigation/native';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+  const navigation = useNavigation();
+
+  function handleOpenGame({ id, title, bannerUrl }:GameCardProps) {
+    navigation.navigate('game', { id, title, bannerUrl });
+  }
+
+  useEffect(() => {
+    fetch('http://192.168.0.103:3333/games')
+    .then(response => response.json())
+    .then(data => setGames(data));
+  }, []);
+
   return (
-    <View style={styles.container}>
+    <Background>
+    <SafeAreaView style={styles.container}>
       <Image
       source={logoImg}
       style={styles.logo}
@@ -19,17 +36,19 @@ export function Home() {
       />
 
       <FlatList 
-        data={GAMES}
+        data={games}
         keyExtractor={item=> item.id}
         renderItem={({item}) => (
           <GameCard
           data={item}
+          onPress={() => handleOpenGame(item)}
           />
         )}
         showsHorizontalScrollIndicator={false}
         horizontal
         contentContainerStyle={styles.contentList}
       />
-    </View>
+    </SafeAreaView>
+    </Background>
   );
 }
